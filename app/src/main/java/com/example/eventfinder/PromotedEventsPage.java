ca1d455f8b58c6d92c51ad26fab6c472ca792bd4
ibnetahir1 <ibnetahir1@gmail.com>
2020-02-27 15:24:11 -0500

    Merging Hamza and Yusen's work
    Sprint 1 UI changes

---
+++ app/src/main/java/com/example/eventfinder/PromotedEventsPage.java
@@ -0,0 +1,15 @@
+package com.example.eventfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PromotedEventsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promoted_events_page);
+        setTitle("Promoted Page");
+    }
+}
