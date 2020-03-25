92b17fa200c6c57545cdb21025257e635c792e0d
yusenjia <yusenjia@buffalo.edu>
2020-03-25 13:05:07 -0400

    Login Page Created

---
+++ sample/src/main/java/com/framgia/sample/calendardayview/LoginPage.java
@@ -0,0 +1,14 @@
+package com.framgia.sample.calendardayview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+        setTitle("Event Finder");
+    }
+}
