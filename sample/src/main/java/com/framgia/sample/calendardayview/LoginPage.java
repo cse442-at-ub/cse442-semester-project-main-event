717ca4f89665822dc87b17e9dacd20a8eeae031a
yusenjia <yusenjia@buffalo.edu>
2020-03-25 14:06:41 -0400

    Configure the (see all Events) botton when clicked goto the main page

--- sample/src/main/java/com/framgia/sample/calendardayview/LoginPage.java
+++ sample/src/main/java/com/framgia/sample/calendardayview/LoginPage.java
@@ -1,7 +1,10 @@
 package com.framgia.sample.calendardayview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class LoginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         setTitle("Event Finder");
+
+        configureSeeAllEvents();
+    }
+
+    //click the see all events botton will open the main page
+    private void configureSeeAllEvents() {
+        //Create a reference to the button
+        Button goToMainButton = (Button) findViewById((R.id.GoToMain));
+
+        goToMainButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent;
+                        intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+
+                    }
+                }
+        );
     }
 }
+
