92745cac63117c0386f050d37fe08407567354a1
jama-ix <jama@classavo.com>
2020-04-01 17:33:00 -0400

    Create_Settings_UI
    Created the UI for the settings page so that users can personalize app

---
+++ sample/src/main/java/com/framgia/sample/calendardayview/Settings.java
@@ -0,0 +1,15 @@
+package com.framgia.sample.calendardayview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        setTitle("Settings");
+    }
+}
