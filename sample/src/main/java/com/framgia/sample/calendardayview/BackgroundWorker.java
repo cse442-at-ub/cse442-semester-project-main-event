d6b34fb6b393ee2a3ba48950731e08451a1c98c3
ibnetahir1 <ibnetahir1@gmail.com>
2020-04-08 15:13:49 -0400

    The Login page now correctly queries the database

--- sample/src/main/java/com/framgia/sample/calendardayview/BackgroundWorker.java
+++ sample/src/main/java/com/framgia/sample/calendardayview/BackgroundWorker.java
@@ -3,6 +3,7 @@ package com.framgia.sample.calendardayview;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.view.View;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -16,40 +17,41 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
+/**
+ * Created by ProgrammingKnowledge on 1/5/2016.
+ */
 public class BackgroundWorker extends AsyncTask<String,Void,String> {
     Context context;
     AlertDialog alertDialog;
-
-    BackgroundWorker(Context ctx) {
+    BackgroundWorker (Context ctx) {
         context = ctx;
     }
-
     @Override
     protected String doInBackground(String... params) {
         String type = params[0];
         String login_url = "https://www-student.cse.buffalo.edu/CSE442-542/2020-spring/cse-442u/login.php";
-        if (type.equals("login")) {
+        if(type.equals("login")) {
             try {
                 String user_name = params[1];
                 String password = params[2];
                 URL url = new URL(login_url);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                String post_data = URLEncoder.encode("user_name", "UTF-8") + "=" + URLEncoder.encode(user_name, "UTF-8") + "&"
-                        + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+                String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
+                        +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
                 outputStream.close();
                 InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
-                String result = "";
-                String line = "";
-                while ((line = bufferedReader.readLine()) != null) {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while((line = bufferedReader.readLine())!= null) {
                     result += line;
                 }
                 bufferedReader.close();
@@ -63,5 +65,22 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
             }
         }
         return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
     }
 }
--- sample/src/main/java/com/framgia/sample/calendardayview/LoginPage.java
+++ sample/src/main/java/com/framgia/sample/calendardayview/LoginPage.java
@@ -6,47 +6,55 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
 
 public class LoginPage extends AppCompatActivity {
+
+    EditText UsernameEt, PasswordEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        configureSeeAllEvent();
-        ConfigRegistration();
+        configureSeeAllEventButton();
+        configureRegisterButton();
 
+        UsernameEt = (EditText)findViewById(R.id.UserName);
+        PasswordEt = (EditText)findViewById(R.id.Password);
     }
 
-    private  void configureSeeAllEvent(){
-        Button gotMain = (Button) findViewById(R.id.GoToMain);
+    private  void configureSeeAllEventButton(){
+        Button goToMain = (Button) findViewById(R.id.GoToMain);
+        goToMain.setOnClickListener(new Button.OnClickListener() {
 
-        gotMain.setOnClickListener(
-                new Button.OnClickListener() {
+                                        public void onClick(View v) {
+                                            Intent intent = new Intent(getApplication(), MainActivity.class);
+                                            startActivity(intent);
+                                        }
 
-                    public void onClick(View v) {
-                        Intent intent = new Intent(getApplication(), MainActivity.class);
-                        startActivity(intent);
-
-                    }
-
-                }
+                                    }
         );
     }
 
-    private  void ConfigRegistration(){
-        Button gotMain = (Button) findViewById(R.id.Register);
+    private  void configureRegisterButton(){
+        Button goToRegister = (Button) findViewById(R.id.Register);
+        goToRegister.setOnClickListener(new Button.OnClickListener() {
 
-        gotMain.setOnClickListener(
-                new Button.OnClickListener() {
+                                            public void onClick(View v) {
+                                                Intent intent = new Intent(getApplication(), RegistrationPage.class);
+                                                startActivity(intent);
+                                            }
 
-                    public void onClick(View v) {
-                        Intent intent = new Intent(getApplication(), RegistrationPage.class);
-                        startActivity(intent);
-
-                    }
-
-                }
+                                        }
         );
     }
+
+    public void OnLogin(View view) {
+        String username = UsernameEt.getText().toString();
+        String password = PasswordEt.getText().toString();
+        String type = "login";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, username, password);
+    }
+
 }
