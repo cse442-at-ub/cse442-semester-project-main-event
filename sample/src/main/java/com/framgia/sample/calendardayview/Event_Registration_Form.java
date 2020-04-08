bebb28c71eaf88efe9a801ddb3988f5accc14fb5
yusenjia <yusenjia@buffalo.edu>
2020-04-07 22:36:57 -0400

    Fixed the layout of the page to look more nicer Add all the icons to the page (all funtions) Bug: won't go to new line when entering the description

--- sample/src/main/java/com/framgia/sample/calendardayview/Event_Registration_Form.java
+++ sample/src/main/java/com/framgia/sample/calendardayview/Event_Registration_Form.java
@@ -1,6 +1,11 @@
 package com.framgia.sample.calendardayview;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -14,6 +19,87 @@ public class Event_Registration_Form extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event__registration__form);
         setTitle("Event Registration Page");
-    }
-}
 
+        configurePromotedButton();
+        configureEventFinderButton();
+        configureCalendarButton();
+        configureEventRegistrationButton();
+        configureProfileButton();
+        configureSettingsButton();
+    }
+
+
+
+    private void configureSettingsButton() {
+        ImageButton Settings = findViewById((R.id.settingsButton));
+        Settings.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Settings.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configureProfileButton() {
+        ImageButton Profile = findViewById((R.id.profileButton));
+        Profile.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Profile.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configureEventRegistrationButton() {
+        ImageButton Register_Event = findViewById((R.id.register_event_button));
+        Register_Event.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Event_Registration_Form.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configurePromotedButton(){
+        //Create a reference to the promoted button
+        ImageButton promotedButton = findViewById((R.id.PromotedButton));
+        //set event listener for the button
+        promotedButton.setOnClickListener(new Button.OnClickListener(){
+                                              @Override
+                                              public void onClick(View v){
+                                                  //this opens the promoted events page when the button is clicked.
+                                                  Intent intent = new Intent(getApplicationContext(),PromotedEventsPage.class);
+                                                  startActivity(intent);
+                                              }
+                                          }
+        );
+    }
+
+    private void configureEventFinderButton(){
+        ImageButton eventFinderButton = findViewById((R.id.EventFinderButton));
+        eventFinderButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),EventFinderPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configureCalendarButton(){
+        ImageButton calendarButton =  findViewById((R.id.CalendarButton));
+        calendarButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: complete this function after the calendar page is created.
+                Intent intent = new Intent(getApplicationContext(),Calender.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    }
