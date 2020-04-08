14a4172ec5a32d07c64ea263626032b36db3259e
yusenjia <yusenjia@buffalo.edu>
2020-04-08 14:52:52 -0400

    All the pages are linked together You can now to go anypage from anypage

--- sample/src/main/java/com/framgia/sample/calendardayview/EventFinderPage.java
+++ sample/src/main/java/com/framgia/sample/calendardayview/EventFinderPage.java
@@ -1,11 +1,16 @@
 package com.framgia.sample.calendardayview;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SearchView;
 
@@ -24,6 +29,12 @@ public class EventFinderPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_finder_page);
         setTitle("Event Finder");
+        configurePromotedButton();
+        configureEventFinderButton();
+        configureCalendarButton();
+        configureEventRegistrationButton();
+        configureProfileButton();
+        configureSettingsButton();
 
         //bind eventList to the ListView on the activity_event_finder_page
         eventList = (ListView) findViewById(R.id.event_list);
@@ -42,6 +53,77 @@ public class EventFinderPage extends AppCompatActivity {
         //binds ListView to adapter
         eventList.setAdapter(adapter);
 
+    }
+
+    private void configureSettingsButton() {
+        ImageButton Settings = findViewById((R.id.settingsButton));
+        Settings.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Settings.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configureProfileButton() {
+        ImageButton Profile = findViewById((R.id.profileButton));
+        Profile.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Profile.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configureEventRegistrationButton() {
+        ImageButton Register_Event = findViewById((R.id.register_event_button));
+        Register_Event.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Event_Registration_Form.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configurePromotedButton() {
+        //Create a reference to the promoted button
+        ImageButton promotedButton = findViewById((R.id.PromotedButton));
+        //set event listener for the button
+        promotedButton.setOnClickListener(new Button.OnClickListener() {
+                                              @Override
+                                              public void onClick(View v) {
+                                                  //this opens the promoted events page when the button is clicked.
+                                                  Intent intent = new Intent(getApplicationContext(), PromotedEventsPage.class);
+                                                  startActivity(intent);
+                                              }
+                                          }
+        );
+    }
+
+    private void configureEventFinderButton(){
+        ImageButton eventFinderButton =  findViewById((R.id.EventFinderButton));
+        eventFinderButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),EventFinderPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configureCalendarButton(){
+        ImageButton calendarButton =  findViewById((R.id.CalendarButton));
+        calendarButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: complete this function after the calendar page is created.
+                Intent intent = new Intent(getApplicationContext(),Calender.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
